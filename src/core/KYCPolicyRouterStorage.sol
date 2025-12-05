dbe479015b6f5633d4407f0f3987bc90173bea9f
kanthub <564080210@qq.com>
2025-12-05 18:50:27 +0800

    fix interface

--- src/core/KYCPolicyRouterStorage.sol
+++ src/core/KYCPolicyRouterStorage.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./interfaces/IOracleKYCPod.sol";
-import "./interfaces/IVerifier.sol";
+import "../interfaces/IOracleKYCPod.sol";
+import "../interfaces/IVerifier.sol";
 
 contract KYCPolicyRouterStorage {
     // ============================ Events ===============================
